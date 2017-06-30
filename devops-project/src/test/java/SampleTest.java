d1780b283d120e5f2f875109d4efa45965449017
chopradeepanshu <chopradeepanshu@gmail.com>
2017-07-01 00:22:13 +0530

    Initial code checkin

---
+++ devops-project/src/test/java/SampleTest.java
@@ -0,0 +1,13 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SampleTest {
+
+	@Test
+	public void simpleTest()  {
+		int a = 10;
+		int b= 20;
+		assertTrue(a+b==300);
+	}
+}
