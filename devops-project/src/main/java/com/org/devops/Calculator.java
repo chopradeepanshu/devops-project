f4861bd1b2bff0c44a8941e1bfc812b569395e1e
chopradeepanshu <chopradeepanshu@gmail.com>
2017-07-05 07:42:44 +0530

    Making changes in the Calculator Prog.

--- devops-project/src/main/java/com/org/devops/Calculator.java
+++ devops-project/src/main/java/com/org/devops/Calculator.java
@@ -1,45 +1,22 @@
 package com.org.devops;
 
 
-//This will create a class in Java
 public class Calculator {
 
-	//This is a method is Java or we can say, a functionality in Java
-	protected void addNumbers(int number1, int number2) {
-		int add = number1 + number2;
-		System.out.println("Addtion of two numbers is : " + add);
-	}
-	
-	//This is a method is Java or we can say, a functionality in Java
-	public void substraction(int number1, int number2) {
-		int diff = number1 - number2;
-		System.out.println("Differnce of two numbers is :  "+diff);
-	}
-	
-	//This is a method is Java or we can say, a functionality in Java
-	public void multiplication(int number1, int number2) {
-		int multiple = number1 * number2;
-		System.out.println("Multiplication of two numbers is :  "+multiple);
+	public int addNumbers(int i, int j) {
+		return i+j;
 	}
 
-	public void division(int number1, int number2) {
-		int division = number1/number2;
-		System.out.println("Dvision of two number is : "+division);
+	public int substractNumber(int i, int j) {
+		return i-j;
 	}
-	
-	// This is a first method from where the Prog will get loaded in Java Memory
-	public static void main(String[] args) {
-		Calculator calc = new Calculator();
-		
-		int number1 = 200;
-		int number2 = 100;
-		
-		calc.addNumbers(number1, number2);
-		calc.substraction(number1, number2);
-		calc.multiplication(number1, number2);
-		calc.division(number1, number2);
+
+	public int multiplyNumber(int i, int j) {
+		return i*j;
 	}
+
+	public int divideNumber(int i, int j) {
+		return i/j;
+	}
+
 }
-
-// As per the request, I have created a new Project in my local system and going to add it on Server Repository.
-
---
+++ devops-project/src/test/java/CalculatorTest.java
@@ -0,0 +1,37 @@
+
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import com.org.devops.Calculator;
+
+public class CalculatorTest {
+
+	//Test Case First and then adding a Functionality
+	@Test
+	public void testAddFunctionalityWithSuccess() {
+		Calculator calculator = new Calculator();
+		int results = calculator.addNumbers(10, 20);
+		assertTrue(results==30);
+	}
+
+	@Test
+	public void testSubstractionFunctionality() {
+		Calculator calc = new Calculator();
+		int results = calc.substractNumber(10, 20);
+		assertTrue(results == -10);
+	}
+	@Test
+	public void testMultiplicationFunctionality() {
+		Calculator calc = new Calculator();
+		int results = calc.multiplyNumber(10, 20);
+		assertTrue(results == 200);
+	}
+	@Test
+	public void testDevisionFunctionality() {
+		Calculator calc = new Calculator();
+		int results = calc.divideNumber(20, 10);
+		assertTrue(results == 2);
+	}
+}
